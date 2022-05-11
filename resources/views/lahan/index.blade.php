08676f8a58350c118be6f0f29a3103e805f06013
dwinrynt <dwinuryantosukatanai@gmail.com>
2022-05-11 13:00:15 +0700

    tambah lahan

---
+++ resources/views/lahan/index.blade.php
@@ -0,0 +1,27 @@
+@extends('mylayouts.main')
+
+@section('container')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark display-4" style="padding: 0 !important;">Lahan Sekolah/ Ketersediaan Lahan Sekolah</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    {{-- main-content --}}
+        
+    {{-- end main-content --}}
+
+    
+
+
+
+
+    
+
+    <div class="content-backdrop fade"></div>
+@endsection
