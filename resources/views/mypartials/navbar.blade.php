7e76919356fedc7077d422e199b2cc69a2eb2e24
jejeV <muejason27@gmail.com>
2022-06-09 14:03:47 +0700

    ambill

--- resources/views/mypartials/navbar.blade.php
+++ resources/views/mypartials/navbar.blade.php
@@ -1,10 +1,3 @@
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-        <path
-            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-    </symbol>
-</svg>
-
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light"
     style="background-color: #00A65B !important;height: 11vh;">
@@ -55,24 +48,24 @@
     <ul class="navbar-nav ml-auto">
         <div class="nav-item container d-block">
             <div class="container p-0 d-flex justify-content-end">
-                <span class="text-white" style="font-size: 20px;font-weight: 600;">{{ Auth::user()->name }}</span>
+                <span class="text-white text-user" style="font-size: 20px; font-weight: 600;">{{ Auth::user()->name }}</span>
             </div>
             @if (Auth::user()->hasRole('dinas'))
-                <span class="text-white text-right badge float-right"
+                <span class="text-white text-right badge float-right text-user"
                     style="background-color:#fcc12d53; border:1.5px solid #fcc12d; font-weight: 600; display:flex;justify-content:center; width:fit-content !important"><i
-                        class="bi bi-info-circle mr-2"></i><span style="font-size: 12px; margin-top:1px;">Masuk Sebagai
+                        class="bi bi-info-circle mr-2"></i><span class="text-user" style="font-size: 12px; margin-top: 1px;">Masuk Sebagai
                         Dinas Pendidikan</span>
                 </span>
             @elseif(Auth::user()->hasRole('kcd'))
-                <span class="text-white text-right badge float-right"
+                <span class="text-white text-right badge float-right text-user"
                     style="background-color:#fcc12d53; border:1.5px solid #fcc12d; font-weight: 600; display:flex;justify-content:center; width:fit-content !important"><i
-                        class="bi bi-info-circle mr-2"></i><span style="font-size: 12px; margin-top:1px;">Masuk Sebagai
+                        class="bi bi-info-circle mr-2"></i><span class="text-user" style="font-size: 12px; margin-top:1px;">Masuk Sebagai
                         Cadisdik</span>
                 </span>
             @else
-                <span class="text-white text-right badge float-right"
+                <span class="text-white text-right badge float-right text-user"
                     style="background-color:#fcc12d53; border:1.5px solid #fcc12d; font-weight: 600; display:flex;justify-content:center; width:fit-content !important"><i
-                        class="bi bi-info-circle mr-2"></i><span style="font-size: 12px; margin-top:1px;">Masuk Sebagai
+                        class="bi bi-info-circle mr-2"></i><span class="text-user" style="font-size: 12px; margin-top:1px;">Masuk Sebagai
                         {{ Auth::user()->getRoleNames()->first() }}</span>
                 </span>
             @endif
