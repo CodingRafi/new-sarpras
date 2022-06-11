2dbd487dcd184e73e3ab80c32e84fbf367632735
CodingRafi <muhamadrafi1408@gmail.com>
2022-06-11 16:15:20 +0700

    sudah

--- app/Models/JenisPimpinan.php
+++ app/Models/JenisPimpinan.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class JenisPimpinan extends Model
 {
@@ -19,5 +21,13 @@ class JenisPimpinan extends Model
 
     public function usulanBangunan(){
         return $this->hasMany(UsulanBangunan::class);
+    }
+
+    public static function belumTerpilih(){
+        return DB::table('jenis_pimpinans as a')->select('a.*')
+                ->leftJoin('pimpinans as b', function($join) {
+                    $join->on('a.id', '=', 'b.jenis_pimpinan_id')
+                        ->where('b.profil_id',  Auth::user()->profil_id);
+                })->whereNull('b.jenis_pimpinan_id')->get();
     }
 }
